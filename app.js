2eef5a81b6c1736f2ffea4679272b559c1a07db4
EmmanueleD <emmanuele.online@gmail.com>
2022-06-04 16:50:39 -0300

    add config database

--- app.js
+++ app.js
@@ -8,26 +8,33 @@ async function main(){
     res.send("Lobby path")
   })
 
-  let port = 3000
+  let port = process.env.PORT || 3590
   app.listen(port, ()=>{
-    console.log("server up at port: "+ port+" [ http://localhost:"+port+' ]')
+    console.log("server up")
   })
 
 
 
   const mysql = require('mysql2/promise')
-  // const bluebird = require('bluebird')
 
+
+//mysql://:@/?reconnect=true
   const connection =  await mysql.createConnection({
-    host: 'localhost',
-    user: 'emmanuele',
-    password: 'password',
-    database: 'recipe-cost-calculator',
-    // Promise: bluebird
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'bb320117d6f6fe',
+    password: 'ddfadcfb',
+    database: 'heroku_d909bf62d8c6a38',
   })
 
+  // const connection =  await mysql.createConnection({
+  //   host: 'us-cdbr-east-05.cleardb.net',
+  //   user: process.env.DB_USER,
+  //   password: process.env.DB_PASS,
+  //   database: process.env.DB_NAME,
+  // })
+
   // GET ALL INGREDIENTS
-  app.get('/api/getIngredients/', async (req, res)=>{
+  app.get('/api/ingredients/', async (req, res)=>{
 
     const result = await connection.execute('SELECT * FROM ingredients')
     console.log(result[0])
@@ -36,14 +43,14 @@ async function main(){
 
   
   // GET INGREDIENT BY ID
-  app.get('/api/getIngredient/:id', async (req, res)=>{
+  app.get('/api/ingredients/:id', async (req, res)=>{
     const result = await connection.execute('SELECT i.id, i.name, i.price, i.qty FROM ingredients as i WHERE i.id = ?', [req.params.id])
     console.log(result[0])
     res.send(result[0])
   })
 
   // POST NEW INGREDIENT
-  app.post('/api/postIngredient/', async(req, res)=>{
+  app.post('/api/ingredients/', async(req, res)=>{
     let data = {
       name: req.body.name,
       price: req.body.price,
@@ -55,7 +62,7 @@ async function main(){
   })
 
   // PUT EXISTING INGREDIENT
-  app.put('/api/putIngredient/:id', async(req, res)=>{
+  app.put('/api/ingredients/:id', async(req, res)=>{
     let data = {
       name: req.body.name,
       price: req.body.price,
@@ -66,20 +73,20 @@ async function main(){
   })
 
   // DELETE EXISTING INGREDIENT
-  app.delete('/api/deleteIngredient/:id', async (req, res)=>{
+  app.delete('/api/ingredients/:id', async (req, res)=>{
 
     const result = await connection.execute('DELETE FROM ingredients AS i WHERE i.id = ?', [req.params.id])
     res.send(result[0])
   })
 
   // GET ALL RECIPES
-  app.get('/api/getRecipes/', async (req, res)=>{
+  app.get('/api/recipeswoingredients/', async (req, res)=>{
     const result = await connection.execute('SELECT * FROM recipes')
     res.send(result[0])
   })
 
   // GET ALL RECIPES WITH INGREDIENTS
-  app.get('/api/getRecipesWIngredients/', async(req, res)=> {
+  app.get('/api/recipes/', async(req, res)=> {
    let arr = []
    //get all ingredients for every recipe 
    const recipesWIngredients = await connection.execute('SELECT * FROM recipes_ingredients')
@@ -110,7 +117,7 @@ async function main(){
   })
 
   // GET RECIPE BY ID
-  app.get('/api/getRecipe/:id', async (req, res)=>{
+  app.get('/api/recipes/:id', async (req, res)=>{
 
     const result = await connection.execute('SELECT r.id AS ID_recipe, r.name AS NAME_recipe, i.id AS ID_ingredient, i.name AS NAME_ingredient, ri.qty AS QTY_ingredient FROM recipes AS r JOIN recipes_ingredients AS ri ON ri.id_recipe = r.id JOIN ingredients AS i ON i.id = ri.id_ingredient WHERE r.id = ?', [req.params.id])
 
@@ -130,7 +137,7 @@ async function main(){
   })
 
   //POST NEW RECIPE
-  app.post('/api/postRecipe/', async (req, res)=>{
+  app.post('/api/recipes/', async (req, res)=>{
     let ingredientsQuery = "INSERT INTO recipes_ingredients (id_recipe, id_ingredient, qty) VALUES"
     let data = {
       name: req.body.name,
@@ -201,7 +208,7 @@ async function main(){
   })
 
   // DELETE EXISTING RECIPE
-  app.delete('/api/deleteRecipe/:id', async (req, res)=> {
+  app.delete('/api/recipes/:id', async (req, res)=> {
     const deleteRecipe = await connection.execute('DELETE FROM recipes WHERE id=?', [req.params.id])
     const deleteIngredients = await connection.execute('DELETE FROM recipes_ingredients WHERE id_recipe=?', [req.params.id])
     res.send({
