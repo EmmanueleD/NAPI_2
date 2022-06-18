ef2fbab7d17fe8d1cf932165103542a8ddbc3fa9
EmmanueleD <emmanuele.online@gmail.com>
2022-06-18 18:12:23 -0300

    database url ignored

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ async function main(){
   const mysql = require('mysql2/promise')
 
   const connection =  await mysql.createPool({
-      host: 'containers-us-west-65.railway.app',
+      host: process.env.DATABASE_URL,
       port:5818,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
